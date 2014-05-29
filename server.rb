0a29baecbb851da0a454581886df765f14dbadeb
Fahad Mukunzi <fahadm507@gmail.com>
2014-05-29 19:24:23 -0400

    added comments

--- server.rb
+++ server.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'pry'
 require 'pg'
 
+#The method that connects us to the news database
 def db_connection
   begin
     connection = PG.connect(dbname:'news')
@@ -11,6 +12,7 @@ def db_connection
   end
 end
 
+#This method takes params hash and inserts its contents to the database.
 def save_post(params)
   sql= 'INSERT INTO posts (title, description, url, created_at)
         VALUES($1,$2,$3,NOW())'
@@ -19,6 +21,7 @@ def save_post(params)
   end
 end
 
+#This method check if the user input is valid
 def check_title_and_description(params)
   if params[:title] == nil
     @errors << "Title can't be empty"
@@ -29,7 +32,8 @@ def check_title_and_description(params)
     @errors << "description can't be less than 20 characters"
   end
 end
-#this method checks if url is valid or not
+
+#This method checks if url is valid or not
 def check_submissions(url)
   url = url.downcase
   if url.include?("http://www.")
@@ -41,6 +45,7 @@ def check_submissions(url)
   end
 end
 
+#This method validate the url before it is stored in the database
 def valid_url?(url)
   begin
     Net::HTTP.get_response(URI(url)).code == "200" ? true : false
@@ -49,10 +54,12 @@ def valid_url?(url)
   end
 end
 
+#This get method redirects the user to the articles page.
 get '/' do
   redirect '/articles'
 end
 
+#Method that gets the articles from the database
 get '/articles' do
 
   sql = 'SELECT * FROM posts'
@@ -63,6 +70,8 @@ get '/articles' do
   erb :index
 end
 
+#This get method gets the users input from form and
+#invokes the save_post method which inserts user's
 post '/articles/new' do
   check_to_url = check_submissions(params[:url])
     if check_to_url == false
